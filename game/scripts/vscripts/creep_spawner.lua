436020d4be7268409b562f209b43b90191611037
pr0crustes <dark.pr0crustes@gmail.com>
2019-07-06 08:04:06 -0300

    Changed a few formulas.

--- game/scripts/vscripts/creep_spawner.lua
+++ game/scripts/vscripts/creep_spawner.lua
@@ -56,7 +56,8 @@ end
 function CreepSpawner:SpawnCreepsAtPos(pos, creep_list, count, round)
 	--print("CreepSpawner:SpawnCreepsAtPos")
 
-	local round_multiplier = 1 + ((round - 1) * 0.1)
+	local round_multiplier = 1 + ((round - 1) * 0.08)
+	local round_multiplier_plus = 1 + ((round - 1) * 0.16)
 
 	for i = 1, count do
 		local creep_name = random_from_table(values_from_dict(creep_list))
@@ -66,10 +67,10 @@ function CreepSpawner:SpawnCreepsAtPos(pos, creep_list, count, round)
 		if creep then
 			creep.is_custom_spawned_creep = true
 
-			creep:SetDeathXP(creep:GetDeathXP() * round_multiplier)
+			creep:SetDeathXP(creep:GetDeathXP() * round_multiplier_plus)
 
-			creep:SetMinimumGoldBounty(creep:GetMinimumGoldBounty() * round_multiplier)
-			creep:SetMaximumGoldBounty(creep:GetMaximumGoldBounty()  * round_multiplier)
+			creep:SetMinimumGoldBounty(creep:GetMinimumGoldBounty() * round_multiplier_plus)
+			creep:SetMaximumGoldBounty(creep:GetMaximumGoldBounty()  * round_multiplier_plus)
 
 			creep:SetMaxHealth(creep:GetMaxHealth() * round_multiplier)
 			creep:SetBaseMaxHealth(creep:GetBaseMaxHealth() * round_multiplier)
@@ -79,7 +80,7 @@ function CreepSpawner:SpawnCreepsAtPos(pos, creep_list, count, round)
 			creep:SetBaseDamageMax(creep:GetBaseDamageMax() * round_multiplier)
 			creep:SetBaseMoveSpeed(creep:GetBaseMoveSpeed() * round_multiplier)
 
-			creep:SetPhysicalArmorBaseValue(creep:GetPhysicalArmorBaseValue() * round_multiplier)
+			creep:SetPhysicalArmorBaseValue(creep:GetPhysicalArmorBaseValue() + ((round - 1) * 0.5))
 		end
 	end
 end
