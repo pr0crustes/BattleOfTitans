f33cd86f88cdb5e7b6963df7e0040844074cb796
pr0crustes <dark.pr0crustes@gmail.com>
2019-07-06 09:17:33 -0300

    Started to working in titan death.

---
+++ game/scripts/vscripts/abilities/units/titan_death.lua
@@ -0,0 +1,13 @@
+
+
+function OnDeath(keys)
+    local unit = keys.unit
+    local caster = keys.caster
+
+    if unit == caster then
+        local pos = unit:GetAbsOrigin()
+
+        
+    end
+end
+
