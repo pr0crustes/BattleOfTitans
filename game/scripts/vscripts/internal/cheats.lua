c6311612c777a460bfce1012132b6b347e035f50
pr0crustes <dark.pr0crustes@gmail.com>
2019-07-21 17:29:50 -0300

    Created noloose cheat.

--- game/scripts/vscripts/internal/cheats.lua
+++ game/scripts/vscripts/internal/cheats.lua
@@ -14,3 +14,15 @@ function Cheats:spawntitans(player, args)
         GameRules.GameMode.titan_countdown = 1
     end
 end
+
+
+function Cheats:noloose(player, args)
+    if GameRules.GameMode then
+        if GameRules.GameMode.ancient_radiant then
+            GameRules.GameMode.ancient_radiant:SetPhysicalArmorBaseValue(99999)
+        end
+        if GameRules.GameMode.ancient_dire then
+            GameRules.GameMode.ancient_dire:SetPhysicalArmorBaseValue(99999)
+        end
+    end
+end
