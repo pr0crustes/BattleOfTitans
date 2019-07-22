f8f42398ee55f8751f436c285a35b24ccfb38a33
pr0crustes <dark.pr0crustes@gmail.com>
2019-07-22 15:49:01 -0300

    More work on bshop.

--- content/panorama/scripts/custom_game/bshop.js
+++ content/panorama/scripts/custom_game/bshop.js
@@ -1,4 +1,4 @@
-CustomNetTables.SubscribeNetTableListener("bshop_update_health_ui", UpdateHealthUI);
+CustomNetTables.SubscribeNetTableListener("bshops", OnBShopChange);
 
 
 var open = false;
@@ -18,14 +18,33 @@ function BuyHealth() {
 	GameEvents.SendCustomGameEventToServer("bshop_event_buy_health", data);
 }
 
-function UpdateHealthUI(table, key, data) {
-    if (!data || key !== "values") {
+
+function UpdateBShop(shop_name, cost, own) {
+    var panel = find_hud_element("buy_" + shop_name);
+    panel.FindChildTraverse("bshop_cost").SetDialogVariable("cost", cost);
+    panel.FindChildTraverse("bshop_own").SetDialogVariable("own", own);
+}
+
+
+function OnBShopChange(table, key, data) {
+    if (!data || key !== "upgrades") {
 		return;
     }
 
-    if (GetPlayerTeam() == data.team) {
-        var panel = find_hud_element("buy_health");
-        panel.FindChildTraverse("bshop_own").text = data.owned;
-        panel.FindChildTraverse("bshop_cost").text = data.cost;
+    $.Msg("OnBShopChange");
+
+    var team_info = data[GetPlayerTeam()];
+    if (team_info) {
+        Object.keys(team_info).forEach(function(key) {
+            var info = team_info[key];
+
+            UpdateBShop(key, info.cost, info.own);
+        });
     }
+
+//    if (GetPlayerTeam() == data.team) {
+//        var panel = find_hud_element("buy_health");
+//        panel.FindChildTraverse("bshop_own").text = data.owned;
+//        panel.FindChildTraverse("bshop_cost").text = data.cost;
+//    }
 }
